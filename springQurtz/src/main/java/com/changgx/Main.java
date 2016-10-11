60808456bb4d07c81934a6a3b216c0727cec8f1b
柳剑 <liujian@kunteng.org>
2016-10-11 15:56:48 +0800

    spring project

---
+++ springQurtz/src/main/java/com/changgx/Main.java
@@ -0,0 +1,15 @@
+package com.changgx;/**
+ * Created by Administrator on 2016/10/11.
+ */
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Administrator 2016/10/11
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext act=new ClassPathXmlApplicationContext("classpath:spring-quartz.xml");
+    }
+}
---
+++ springQurtz/src/main/java/com/changgx/MyQuartz.java
@@ -0,0 +1,14 @@
+package com.changgx;/**
+ * Created by Administrator on 2016/10/11.
+ */
+
+import java.util.Date;
+
+/**
+ * Administrator 2016/10/11
+ */
+public class MyQuartz {
+    public void print(){
+        System.out.println(new Date());
+    }
+}
